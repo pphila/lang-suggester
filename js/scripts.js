e51096fe027775f7a7c76d224be6dc319afbe88e
pphila <efl2ice89@gmail.com>
2022-08-27 12:23:46 -0700

    add reset btn and javascript functionailty to clear form and hide result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,10 @@
-//User Interface Logic
-//window.addEventListener("load", function() {
-  //let form = document.querySelector("form");
-  //let result = document.getElementById("result")
 
-  //form.addEventListener("submit", function(event) {
-    //event.preventDefault();
-    //const answer1 = parseInt.document.querySelector("input[name='question1Rad']:chekced").value;
-    //const answer2 = parseInt.document.querySelector("input[name='question2Rad']:checked").value;
-    //const answer3 = parseInt.document.querySelector("input[name='question3Rad']:checked").value;
-    //const answer4 = parseInt.document.querySelector("input[name='question4Rad']:checked").value;
-    //const answer5 = parseInt.document.querySelector("input[name='question5Rad']:checked").value;
-    //const score = answer1 + answer2 + answer3 + answer4 + answer5;
-  
-    //result.removeAttribure("class");
-    //if (score >= 6) {
-      //document.querySelector("#resultCSharp").removeAttribute("class");
-    //} else if (score <6 && score >= 4) {
-      //document.querySelector("#resultPython").removeAttrubute("class");
-    //} else if (score < 4) {
-      //document.querySelector("#resultJavaScript").removeAttribute("class");
-    //} else {
-      //document.querySelector("#resultNone").removeAttribute("class");
-    //}
-  //});
-//});
-
-window.onload = function() {
+window.addEventListener("load", function() {
   const form = document.querySelector("form");
-  form.onsubmit = function(event) {
+  let resetBtn = document.getElementById("reset");
+  let result = document.getElementById("results")
+  form.addEventListener("submit", function(event) {
+    result.removeAttribute("class");
     event.preventDefault();
 
     let answer1 = parseInt(document.querySelector("input[name='question1Rad']:checked").value);
@@ -44,12 +21,35 @@ window.onload = function() {
 
     if (score >= 6) {
       document.getElementById("resultCSharp").removeAttribute("class");
+      document.getElementById("resultPython").setAttribute("class", "hidden");
+      document.getElementById("resultJavaScript").setAttribute("class", "hidden");
     } else if (score < 6 && score >= 4) {
       document.getElementById("resultPython").removeAttribute("class");
-    } else if (score < 4 || score === 0) {
+      document.getElementById("resultCSharp").setAttribute("class", "hidden");
+      document.getElementById("resultJavaScript").setAttribute("class", "hidden");
+    } else if (score < 4 && score >= 0) {
       document.getElementById("resultJavaScript").removeAttribute("class");
+      document.getElementById("resultCSharp").setAttribute("class", "hidden");
+      document.getElementById("resulrPython").setAttribute("class", "hidden");
     } else {
       document.getElementById("resulNone").removeAttribute("class");
+      document.getElementById("resultCSharp").setAttribute("class", "hidden");
+      document.getElementById("resulrPython").setAttribute("class", "hidden");
+      document.getElementById("resultJavaScript").setAttribute("class", "hidden");
     }
-  };
-};
+  });
+
+  form.addEventListener("submit", function() {
+    resetBtn.removeAttribute("class");
+  });
+
+  resetBtn.addEventListener("click", function() {
+    result.setAttribute("class", "hidden");
+    document.querySelector("form").clearCheck();
+    document.querySelector("input[name='question1Rad']").value = null;
+    document.querySelector("input[name='question2Rad']").value = null;
+    document.querySelector("input[name='question3Rad']").value = null;
+    document.querySelector("input[name='question4Rad']").value = null;
+    document.querySelector("input[name='question5Rad']").value = null;
+  });
+});
